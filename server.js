237eca46d3e34a73b5d8ec9f0610a7264f5bf25d
CptKirkee <50964250+CptKirkee@users.noreply.github.com>
2021-01-02 21:07:29 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+/*server.js
+'use strict';
+
+const fs = require('fs');
+let student = {
+	name: 'Mike',
+	city: 'Berlin',
+	profession: 'idiot',
+	age: 23,
+};
+let data = JSON.stringify(student);
+fs.writeFileSync('student-2.json', data);
+*/
+
+setTimeout(function(){ 
+
+  console.log(document.getElementsByTagName("p")[0]);
+
+}, 3000);
